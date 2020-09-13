156d9f5a88cf69177615cddbe531f73fc8232cc7
Javier Ramirez <lorlgd1@gmail.com>
2020-09-13 09:26:23 -0500

    JaRa: Implemented exercise 3. Overriden arithmetic operators to support operations with fractions

--- Fraction.py
+++ Fraction.py
@@ -16,6 +16,22 @@ class Fraction:
         new_den = self.den * other_fraction.den
         return Fraction(new_num, new_den)
 
+    def __sub__(self, other_fraction):
+        new_num = self.num * other_fraction.den - \
+            self.den * other_fraction.num
+        new_den = self.den * other_fraction.den
+        return Fraction(new_num, new_den)
+
+    def __mul__(self, other_fraction):
+        new_num = self.num * other_fraction.num
+        new_den = self.den * other_fraction.den
+        return Fraction(new_num, new_den)
+
+    def __truediv__(self, other_fraction):
+        new_num = self.num * other_fraction.den
+        new_den = self.den * other_fraction.num
+        return Fraction(new_num, new_den)
+
     def __eq__(self, other):
         first_num = self.num * other.den
         second_num = other.num * self.den
@@ -29,11 +45,15 @@ class Fraction:
 
 
 def gcd(m, n):
-    while m%n != 0:
+    while m % n != 0:
         old_m = m
         old_n = n
         m = old_n
         n = old_m % old_n
     return n
 
-f1= Fraction(6,8)
+
+f1 = Fraction(3, 4)
+f2 = Fraction(5, 9)
+f3 = f1 / f2
+print(f3)
