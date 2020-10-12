572294218ed94b04af523bfbb5fff36c478f2823
Javier Ramirez <lorlgd1@gmail.com>
2020-10-12 14:17:06 -0500

    JaRa: Added AndGate logic

---
+++ LogicGatesLesson/LogicGates/UnaryGate.py
@@ -0,0 +1,11 @@
+from LogicGates.LogicGate import LogicGate
+
+
+class UnaryGate(LogicGate):
+
+    def __init__(self, n):
+        super().__init__(n)
+        self.pin = None
+
+    def getPin(self):
+        return int(input("Enter Pin input for gate " + self.getLabel() + "-->"))
