a2b32ebfcebe6577e086e87f09417e36725ba995
Javier Ramirez <lorlgd1@gmail.com>
2020-10-18 19:41:05 -0500

    JaRa: Added OR, Not and Connector classes.

---
+++ LogicGatesLesson/LogicGates/OrGate.py
@@ -0,0 +1,13 @@
+from LogicGates.BinaryGate import BinaryGate
+
+class OrGate(BinaryGate):
+    def __init__(self, n):
+        super().__init__(n)
+
+    def performGateLogic(self):
+        a = self.getPinA()
+        b = self.getPinB()
+        if a == 0 and b == 0:
+            return 0
+        else:
+            return 1
